3c4c0b45883e9d71bf69ac589c2ad9ad476c35fb
JPNaik <jpnaik48@gmail.com>
2026-09-05 15:25:51 +0530

    added selenium grid code

--- src/main/java/com/flynas/utils/BaseUtils.java
+++ src/main/java/com/flynas/utils/BaseUtils.java
@@ -4,13 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.time.Duration;
 
 public class BaseUtils {
@@ -20,11 +26,37 @@ public class BaseUtils {
         return driverContainer.get();
     }
     @BeforeMethod
-    @Parameters({"browser"})
-    public void launchURL(@Optional String xmlBrowser){
+    @Parameters({"browser","executionPlatform"})
+    public void launchURL(@Optional String xmlBrowser,@Optional String executionPlatform) throws MalformedURLException {
         String targetBrowser = (xmlBrowser!=null)?xmlBrowser.toLowerCase().trim():ConfigReader.getProperty("browser").toLowerCase().trim();
+        String platform = (executionPlatform != null && !executionPlatform.isEmpty())
+                ? executionPlatform.toLowerCase().trim()
+                : ConfigReader.getProperty("execution.platform").toLowerCase().trim();
         WebDriver driver;
-        switch (targetBrowser){
+        if (platform.equalsIgnoreCase("grid")) {
+            URL gridUrl = URI.create(ConfigReader.getProperty("selenium.grid.url")).toURL();
+
+            switch (targetBrowser) {
+                case "firefox":
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    //if (isHeadless) firefoxOptions.addArguments("-headless");
+                    driver = new RemoteWebDriver(gridUrl, firefoxOptions);
+                    break;
+
+                case "edge":
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                   // if (isHeadless) edgeOptions.addArguments("--headless=new");
+                    driver = new RemoteWebDriver(gridUrl, edgeOptions);
+                    break;
+
+                case "chrome":
+                default:
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    driver = new RemoteWebDriver(gridUrl, chromeOptions);
+                    break;
+            }
+        }else{
+        switch (targetBrowser) {
             case "firefox":
                 driver = new FirefoxDriver();
                 break;
@@ -34,7 +66,7 @@ public class BaseUtils {
             case "chrome":
             default:
                 ChromeOptions options = new ChromeOptions();
-                if(ConfigReader.getProperty("headless").equalsIgnoreCase("true")){
+                if (ConfigReader.getProperty("headless").equalsIgnoreCase("true")) {
                     options.addArguments("--headless=new");
                     options.addArguments("--window-size=1920,1080");
                     options.addArguments("--disable-gpu");
@@ -44,6 +76,7 @@ public class BaseUtils {
                 }
                 driver = new ChromeDriver(options);
                 break;
+        }
         }
         int implicitWaitTimeOut  = Integer.parseInt(ConfigReader.getProperty("implicit.wait"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWaitTimeOut));
